9a60ca76536f580840d51f001518e17dbb32c51e
kossai-amharrar <amhrar46@gmail.com>
2022-12-17 00:58:37 +0100

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Back from "../common/Back"
+import Heading from "../common/Heading"
+import img from "../images/about.jpg"
+import "./about.css"
+
+const About = () => {
+  return (
+    <>
+      <section className='about'>
+        <Back name='About Us' title='About Us - Who We Are?' cover={img} />
+        <div className='container flex mtop'>
+          <div className='left row'>
+            <Heading title='Our Agency Story' subtitle='Check out our company story and work process' />
+
+            <p>RentUP is the augmented broker that radically simplifies the purchase of real estate. Whatever your project, we have redesigned the experience around caring support and the best online tools. Forget the obstacle course, it's up to you to control the real estate purchase.</p>
+            <p></p>
+            <button className='btn2'>More About Us</button>
+          </div>
+          <div className='right row'>
+            <img src='./immio.jpg' alt='' />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default About
